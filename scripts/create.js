2dd7154335fcd2a39accbec04d603c4c5c291920
Ranjan Shrestha <ranjan@sharingapples.com>
2018-09-02 22:34:28 +0545

    Move file copying utility as a separate module

--- scripts/create.js
+++ scripts/create.js
@@ -1,39 +1,11 @@
 const fs = require('fs');
 const path = require('path');
+const copyFiles = require('./_recursiveCopy');
 
 const textFiles = [
   '.ts', '.js', '.json', '.txt', '.html', '.css', '.less',
   '.java', '.m', '.h', '.plist', '.ini',
 ];
-
-function copyFiles(sourceFolder, targetFolder, variables) {
-  const files = fs.readdirSync(sourceFolder);
-  console.log(files);
-  files.forEach((file) => {
-    // Template files begin with '__'
-    if (!file.startsWith('__')) {
-      return;
-    }
-
-    const sourceFile = path.resolve(sourceFolder, file);
-    const targetFile = path.resolve(targetFolder, file.substr(2));
-    if (fs.lstatSync(sourceFile).isDirectory()) {
-      fs.mkdirSync(targetFile);
-      copyFiles(sourceFile, targetFile, variables);
-      return;
-    }
-
-    // Just create a copy if its not a test file
-    if (!textFiles.some(t => sourceFile.endsWith(t))) {
-      fs.copyFileSync(sourceFile, targetFile);
-    } else {
-      const data = fs.readFileSync(sourceFile, 'utf-8');
-      // perform a regular expression replace from variables
-      const content = data.replace(/\{\{([^{]*)\}\}/mg, (match, key) => variables[key]);
-      fs.writeFileSync(targetFile, content, "utf-8");
-    }
-  });
-}
 
 module.exports = function (config, package, args) {
   // Init will create an index.js and a package.json file.
